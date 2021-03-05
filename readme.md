9448636af9e7c1bcfc70c1a49b04ac93d533c8af
olix3001 <olix3001@gmail.com>
2021-03-05 22:01:21 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+1. pobierz node.js z nodejs.org
+2. wpisz w konsolce `npm i git+https://github.com/olix3001yt/oij-automatic-tests.git -g`
+3. wejdź w folder z generatorem testów, rozwiązaniem bruteforce oraz twoim rozwiązaniem
+4. wpisz w konsolce poniższe polecenie
+`tester test <nazwa pliku z twoim rozwiązaniem> <nazwa pliku z rozwiązaniem bruteforce> <nazwa pliku generatora testów>`
+lub jeżeli chcesz wykonać inną ilość testów dopisz na końcu `-c <ilość testów>`
+a jeżeli chcesz usunąć folder 'generated-tests' po zakończeniu testów, dopisz flagę `-r`
---
+++ src/tester.js
@@ -0,0 +1,151 @@
+import commander from 'commander';
+import Listr from 'listr';
+import fs from 'fs';
+import path from 'path';
+import execa from 'execa';
+import { Observable } from 'rxjs';
+import chalk from 'chalk';
+
+commander.command('test <program> <bruteforce> <generator>')
+    .description('test your python program')
+    .option('-c --count <count>', 'how many tests', 10)
+    .option('-r --remove', 'remove directory after testing')
+    .action(async (program, bruteforce, generator, options) => {
+        let tasks = [];
+
+        // create tests directory
+        await fs.mkdir(path.join(process.cwd(), 'generated-tests'), (err) => {
+            if (err) {
+                return console.error(err);
+            }
+        });
+        let testsPath = path.join(process.cwd(), 'generated-tests');
+
+        //create error list
+        let errors = [];
+
+        // create tests
+        for (let i = 0; i < parseInt(options.count); i++) {
+            tasks.push({
+                title: `Test ${i}`,
+                task: () => {
+                    return new Observable(async observer => {
+                        // generate test
+                        let subprocess = null;
+                        try {
+                            observer.next('Generating test');
+                            subprocess = execa('python', [generator]);
+                            subprocess.stdout.pipe(fs.createWriteStream(path.join(testsPath, `test-${i}.in`)));
+                            await subprocess;
+                        } catch (err) {
+                            errors.push({
+                                type: 'generation',
+                                error: err,
+                                num: i
+                            });
+                            await subprocess.cancel();
+                            observer.complete();
+                            return;
+                        }
+
+                        // run bruteforce version
+                        try {
+                            observer.next('Running bruteforce');
+                            subprocess = execa('python', [bruteforce]);
+                            subprocess.stdout.pipe(fs.createWriteStream(path.join(testsPath, `bruteforce-${i}.out`)));
+                            fs.createReadStream(path.join(testsPath, `test-${i}.in`)).pipe(subprocess.stdin);
+                            await subprocess;
+                        } catch (err) {
+                            errors.push({
+                                type: 'bruteforce',
+                                error: err,
+                                num: i
+                            });
+                            await subprocess.cancel();
+                            observer.complete();
+                            return;
+                        }
+
+                        // run normal version
+                        try {
+                            observer.next('Running your program');
+                            subprocess = execa('python', [program]);
+                            subprocess.stdout.pipe(fs.createWriteStream(path.join(testsPath, `program-${i}.out`)));
+                            fs.createReadStream(path.join(testsPath, `test-${i}.in`)).pipe(subprocess.stdin);
+                            await subprocess;
+                        } catch (err) {
+                            errors.push({
+                                type: 'program',
+                                error: err,
+                                num: i
+                            });
+                            await subprocess.cancel();
+                            observer.complete();
+                            return;
+                        }
+
+
+                        // compare results
+                        fs.readFile(path.join(testsPath, `bruteforce-${i}.out`), (err, data) => {
+                            if (err) {
+                                errors.push({
+                                    type: 'compare',
+                                    error: err,
+                                    num: i
+                                });
+                            }
+
+                            fs.readFile(path.join(testsPath, `program-${i}.out`), (err, data1) => {
+                                if (err) {
+                                    errors.push({
+                                        type: 'compare',
+                                        error: err,
+                                        num: i
+                                    });
+                                }
+
+                                if (!data.equals(data1)) {
+                                    errors.push({
+                                        type: 'result',
+                                        error: '---->Got:\n' + data1 + '\n---->But Expected:\n' + data,
+                                        num: i
+                                    });
+                                }
+                            })
+                        })
+
+                        observer.complete();
+                    });
+                }
+            });
+        }
+
+        await new Listr(tasks).run().catch((err) => {
+            console.error(err);
+            return;
+        });
+
+        for (let err of errors) {
+            console.log('\n');
+            console.log(chalk.yellow(`Test id: Test ${err.num}`))
+            if (err.type == 'generation') {
+                console.log(chalk.red.bold('Trror while generating test'));
+            } else if (err.type == 'bruteforce') {
+                console.log(chalk.red.bold('Error while running bruteforce'));
+            } else if (err.type == 'program') {
+                console.log(chalk.red.bold('Error while running your program'));
+            } else if (err.type = 'result') {
+                console.log(chalk.red.bold('Bad result'));
+            } else {
+                console.log(chalk.red.bold('Error while comparing results'));
+            }
+            console.log(chalk.red(err.error));
+        }
+
+        if (options.remove) {
+            fs.rmSync(testsPath, { recursive: true, force: true });
+        }
+
+    });
+
+commander.parse(process.argv);
